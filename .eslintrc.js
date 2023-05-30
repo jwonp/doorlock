7bc04c90d4f489cfe3525583730472a60f3a7038
Joowon <tkdel222@gmail.com>
2023-05-30 17:47:20 +0900

    update settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  plugins: ['react', '@typescript-eslint'],
+  extends: [
+    'eslint:all',
+    'plugin:react',
+    'plugin:@typescript-eslint/recommended',
+    '@react-native-community',
+  ],
+  rules: {
+    'no-unused-vars': 'off',
+    'no-unescaped-entities': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unescaped-entities': 'off',
+  },
 };
