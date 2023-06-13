8f761c6a893f5865196beb0ce30fd1958e155687
Joowon <tkdel222@gmail.com>
2023-06-13 19:37:20 +0900

    update style sheet

---
+++ src/assets/list/ListBarStyleSheet.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+export const styles = StyleSheet.create({
+  text: {
+    color: '#ffffff',
+  },
+});
+export const listStyles = StyleSheet.create({
+  itemContainer: {
+    backgroundColor: '#3c3c3c',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ffffff',
+    padding: '4%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
