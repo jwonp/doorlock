8906b8dab04137bd79f9d7f0128b9fe0e67a44b7
Joowon <tkdel222@gmail.com>
2023-06-15 20:49:43 +0900

    fix state user,room select .. to just selectM..V..

--- src/redux/features/modalState.ts
+++ src/redux/features/modalState.ts
@@ -4,10 +4,12 @@ import {RootState} from '../store';
 
 export interface ModalState {
   newUserModalVisible: boolean;
+  selectModalVisible: boolean;
 }
 
 const initialState: ModalState = {
   newUserModalVisible: false,
+  selectModalVisible: false,
 } as const;
 
 export const modal = createSlice({
@@ -17,11 +19,17 @@ export const modal = createSlice({
     setNewUserModalVisible: (state, action: PayloadAction<boolean>) => {
       state.newUserModalVisible = action.payload;
     },
+    setSelectModalVisible: (state, action: PayloadAction<boolean>) => {
+      state.selectModalVisible = action.payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const {setNewUserModalVisible} = modal.actions;
+export const {setNewUserModalVisible, setSelectModalVisible} = modal.actions;
 export const getNewUserModalVisible = (state: RootState) =>
   state.modal.newUserModalVisible;
+export const getSelectModalVisible = (state: RootState) =>
+  state.modal.selectModalVisible;
+
 export default modal.reducer;
