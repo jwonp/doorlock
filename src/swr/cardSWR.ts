c9dc3b6b5c00f952744e3669d00842f37585ae4e
Joowon <tkdel222@gmail.com>
2023-06-17 11:32:25 +0900

    refactor select data SWR fecher

--- src/swr/cardSWR.ts
+++ src/swr/cardSWR.ts
@@ -12,18 +12,11 @@ export const CardListURL = () => {
   return `${Config.BACKEND_ENDPOINT}/card`;
 };
 
-export const CardSelectDataFetcher = (
+export const SelectDataFetcher = (
   url: string,
-  type: ModalType,
-): Promise<UserListResponse[] | RoomListResponse[]> => {
-  return axios
-    .get(url)
-    .then(res =>
-      type === ModalType.user
-        ? (res.data as UserListResponse[])
-        : (res.data as RoomListResponse[]),
-    );
-};
+): Promise<RoomListResponse[] | UserListResponse[]> =>
+  axios.get(url).then(res => res.data);
+
 export const CardSelectDataURL = (type: ModalType) => {
   return `${Config.BACKEND_ENDPOINT}/${type}`;
 };
