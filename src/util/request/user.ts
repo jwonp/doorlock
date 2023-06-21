e291083fe8e820dea9d5684efbc6fd4c9c209161
Joowon <tkdel222@gmail.com>
2023-06-21 20:07:30 +0900

    update requests to modify data for select modal

--- src/util/request/user.ts
+++ src/util/request/user.ts
@@ -1,8 +1,32 @@
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 import {UserAddRequest} from '../../assets/models/dto/user/UserAddRequest';
 import {request} from './controller/ApiController';
 import Config from 'react-native-config';
+import {
+  UserModifyRoomRequest,
+  UserModifyCardRequest,
+} from '../../assets/models/dto/user/UserModifyRequest';
 
 export const addUser = async (user: UserAddRequest) => {
   return await request.post(`${Config.BACKEND_ENDPOINT}/user`, user);
+};
+export const modifyCardidInUser = async (
+  cardId: string,
+  userId: string,
+): Promise<AxiosResponse<boolean, any>> => {
+  const CardIdModifyData: UserModifyCardRequest = {
+    userId: userId,
+    cardId: cardId,
+  };
+  return await request.patch('/user/card', CardIdModifyData);
+};
+export const modifyRoomidInUser = async (
+  roomId: number,
+  userId: string,
+): Promise<AxiosResponse<boolean, any>> => {
+  const UserIdModifyData: UserModifyRoomRequest = {
+    userId: userId,
+    roomId: roomId,
+  };
+  return await request.patch('/user/room', UserIdModifyData);
 };
