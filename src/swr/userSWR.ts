e5fe614a2a59d97b1c47258c099167424e577316
Joowon <tkdel222@gmail.com>
2023-06-29 21:48:38 +0900

    update user swr

--- src/swr/userSWR.ts
+++ src/swr/userSWR.ts
@@ -2,8 +2,18 @@ import axios from 'axios';
 import Config from 'react-native-config';
 
 import {UserListResponse} from '../assets/models/dto/user/UserListResponse';
+import {UserDataState} from '../redux/features/userState';
+import { request } from '../util/request/controller/ApiController';
 export const UserListFetcher = (url: string): Promise<UserListResponse[]> =>
-  axios.get(url).then(res => res.data);
+  request.get(url).then(res => res.data);
+
 export const UserListURL = () => {
-  return `${Config.BACKEND_ENDPOINT}/user`;
+  return `${Config.BACKEND_ENDPOINT}/user/list`;
+};
+
+export const SelectedUserFetcher = (url: string): Promise<UserDataState> =>
+  axios.get(url).then(res => res.data);
+
+export const selectUserURL = (userId: string) => {
+  return `${Config.BACKEND_ENDPOINT}/user?id=${userId}`;
 };
