f1058f99d1ca9588089f25ff4d9379d9f2b88353
Joowon <tkdel222@gmail.com>
2023-06-29 21:48:29 +0900

    update card swr

--- src/swr/cardSWR.ts
+++ src/swr/cardSWR.ts
@@ -1,22 +1,11 @@
-import axios from 'axios';
 import Config from 'react-native-config';
-
-import {CardListResponse} from '../assets/models/dto/card/CardListResponse';
-import {ModalType} from '../redux/features/modalTypeState';
-import {UserListResponse} from '../assets/models/dto/user/UserListResponse';
-import {RoomListResponse} from '../assets/models/dto/room/RoomListResponse';
-export const CardListFetcher = (url: string): Promise<CardListResponse[]> =>
-  axios.get(url).then(res => res.data);
+import {CardWithReservationResponse} from '../assets/models/dto/card/CardWithReservationResponse';
+import {request} from '../util/request/controller/ApiController';
+export const CardListFetcher = (
+  url: string,
+): Promise<CardWithReservationResponse[]> =>
+  request.get(url).then(res => res.data);
 
 export const CardListURL = () => {
-  return `${Config.BACKEND_ENDPOINT}/card`;
-};
-
-export const SelectDataFetcher = (
-  url: string,
-): Promise<RoomListResponse[] | UserListResponse[]> =>
-  axios.get(url).then(res => res.data);
-
-export const CardSelectDataURL = (type: ModalType) => {
-  return `${Config.BACKEND_ENDPOINT}/${type}`;
+  return `${Config.BACKEND_ENDPOINT}/card/list/reservation`;
 };
