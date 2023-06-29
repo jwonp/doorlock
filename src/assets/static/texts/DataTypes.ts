43b3dc539d4781a8229cef08bab3affa55777749
Joowon <tkdel222@gmail.com>
2023-06-29 21:48:11 +0900

    add reservation in data type set

--- src/assets/static/texts/DataTypes.ts
+++ src/assets/static/texts/DataTypes.ts
@@ -1,11 +1,13 @@
 export const CARD = 'card';
 export const USER = 'user';
 export const ROOM = 'room';
+export const RESERVATION = 'reservation';
 
 const dataTypeSet = {
   card: CARD,
   user: USER,
   room: ROOM,
+  reservation:RESERVATION
 } as const;
 
 export type DataTypes = (typeof dataTypeSet)[keyof typeof dataTypeSet];
