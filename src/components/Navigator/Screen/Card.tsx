a5e3ac1ff8119a950176f1871aa7a7d6867a413b
Joowon <tkdel222@gmail.com>
2023-07-12 22:39:57 +0900

    replace Card Screen

--- src/components/Navigator/Screen/Card.tsx
+++ src/components/Navigator/Screen/Card.tsx
@@ -1,63 +1,28 @@
-import {View, Text, StyleSheet, GestureResponderEvent} from 'react-native';
+import {Text, ScrollView} from 'react-native';
 import useSWR from 'swr';
 import {CardListFetcher, CardListURL} from '@/swr/cardSWR';
 import {useMemo} from 'react';
-import ListBar from '@/assets/list/data/ListBar';
-import {CARD} from '@/assets/static/texts/DataTypes';
-import DataViewContainer from '@/assets/views/data/DataViewContainer';
-import DataView from '@/assets/views/data/DataView';
-import ListContainer from '@/assets/views/data/ListContainer';
-import {useAppDispatch, useAppSelector} from '@/redux/hooks';
-import {getSelectedCard, setSelectedCard} from '@/redux/features/cardState';
-import TechTypeDataView from '@/assets/views/data/card/TechTypeDataView';
-import {screenStyles} from '@/assets/screen/ScreenStylyeSheet';
-import {styles} from '@/assets/list/data/ListBarStyleSheet';
-import {getCardReservationId} from '@/util/convertDisplayText';
 
+import {useAppDispatch, useAppSelector} from '@/redux/hooks';
+import {getSelectedCard} from '@/redux/features/cardState';
+
+import {screenStyles} from '@/assets/screen/ScreenStylyeSheet';
+
+import CardListBar from '@/assets/list/data/card/CardListBar';
 const CardScreen = () => {
   const card = useAppSelector(getSelectedCard);
   const dispatch = useAppDispatch();
   const cardListSWR = useSWR(CardListURL, CardListFetcher);
-  const CardListBar = useMemo(() => {
+  const ListBar = useMemo(() => {
     if (!cardListSWR || !cardListSWR.data) {
-      return <Text style={styles.text}>No user list</Text>;
+      return <Text style={{color: '#ffffff'}}>No user list</Text>;
     }
-
-    return cardListSWR.data.map((item, index) => {
-      return (
-        <ListBar
-          key={index}
-          type={CARD}
-          data={item}
-          index={index}
-          onPress={(event: GestureResponderEvent) => {
-            dispatch(setSelectedCard(item));
-          }}
-        />
-      );
-    });
+    return cardListSWR.data.map((item, index) => (
+      <CardListBar key={index} data={item} />
+    ));
   }, [cardListSWR.data]);
 
-  return (
-    <View style={screenStyles.container}>
-      <DataViewContainer>
-        <DataView label={'Card ID'} text={card.id} />
-        <DataView label={'Max Size'} text={card.maxSize} />
-        <DataView label={'Type'} text={card.type} />
-        <DataView label={'Tech Type'}>
-          <TechTypeDataView techType={card.techType} />
-        </DataView>
-
-        <DataView
-          label={'Reservation Id'}
-          text={getCardReservationId(card.id, card.reservationId)}
-        />
-      </DataViewContainer>
-      <ListContainer title={CARD} height={45}>
-        {CardListBar}
-      </ListContainer>
-    </View>
-  );
+  return <ScrollView style={screenStyles.container}>{ListBar}</ScrollView>;
 };
 
 export default CardScreen;
