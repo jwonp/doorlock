408848c3dc1b20c6b6282e7144ce4ac8451b64ac
Joowon <tkdel222@gmail.com>
2023-07-17 20:00:12 +0900

    fix file name typo

--- src/assets/screen/ScreenStyleSheet.ts
+++ src/assets/screen/ScreenStyleSheet.ts
@@ -1,6 +1,11 @@
 import {StyleSheet} from 'react-native';
 export const screenStyles = StyleSheet.create({
-  container: {padding: '3%', backgroundColor: '#9BABB8', overflow: 'hidden'},
+  container: {
+    padding: '3%',
+    backgroundColor: '#967E76',
+    height: '100%',
+    overflow: 'hidden',
+  },
   whiteText: {
     color: '#ffffff',
   },
