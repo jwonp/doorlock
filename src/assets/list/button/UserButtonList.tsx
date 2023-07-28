712e8ac9cf9f71dfd85d366f11c38e4528676335
Joowon <tkdel222@gmail.com>
2023-07-28 20:59:28 +0900

    update DeleteButton

--- src/assets/list/button/UserButtonList.tsx
+++ src/assets/list/button/UserButtonList.tsx
@@ -1,16 +1,19 @@
 import ModalOpenButton from '@/assets/buttons/ModalOpenButton';
 import DeleteButton from '@/assets/buttons/DeleteButton';
-import {ROOM, USER} from '@/assets/static/texts/DataTypes';
-import {setRoomAddModalVisible} from '@/redux/features/modal/modalState';
+import {setUserAddModalVisible} from '@/redux/features/modal/modalState';
 import {View} from 'react-native';
 import {buttonListStyles} from './ButtonListStyles';
 import UserAddIcon from '@/public/user-add.png';
+import {USER} from '@/assets/static/texts/DataTypes';
 const UserButtonList = () => {
   return (
     <View style={buttonListStyles.container}>
+      <View>
+        <DeleteButton type={USER} />
+      </View>
       <View style={buttonListStyles.card}>
         <ModalOpenButton
-          setModalVisible={setRoomAddModalVisible}
+          setModalVisible={setUserAddModalVisible}
           iconSource={UserAddIcon}
         />
       </View>
