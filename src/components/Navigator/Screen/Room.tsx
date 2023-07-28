bdba5910bf949caebe74c4609733c26efc767f08
Joowon <tkdel222@gmail.com>
2023-07-28 21:03:14 +0900

    update Selectable to ListBar

--- src/components/Navigator/Screen/Room.tsx
+++ src/components/Navigator/Screen/Room.tsx
@@ -1,30 +1,23 @@
-import {View, Text, GestureResponderEvent} from 'react-native';
+import {View, Text} from 'react-native';
 import useSWR from 'swr';
 import {RoomListFetecher, RoomListURL} from '@/swr/roomSWR';
-import DataViewContainer from '@/assets/list/data/DataViewContainer';
-import DataView from '@/assets/list/data/DataView';
-import {useAppDispatch, useAppSelector} from '@/redux/hooks';
-import {getSelectedRooms} from '@/redux/features/roomState';
 import {useMemo} from 'react';
-import ListContainer from '@/assets/list/data/ListContainer';
-import {ROOM} from '@/assets/static/texts/DataTypes';
-
-import {screenStyles} from '@/assets/screen/ScreenStylyeSheet';
-import ReservationsView from '@/assets/list/data/room/ReservationsView';
+import {screenStyles} from '@/assets/screen/ScreenStyleSheet';
 import RoomListBar from '@/assets/list/data/room/RoomListBar';
 import RoomAddModal from '@/components/Modal/Room/RoomAddModal';
+import {ROOM} from '@/assets/static/texts/DataTypes';
+import Selectable from '@/assets/list/data/Selectable';
 
 const RoomScreen = ({navigation}: {navigation: any}) => {
-  const room = useAppSelector(getSelectedRooms);
-  const dispatch = useAppDispatch();
   const roomListSWR = useSWR(RoomListURL, RoomListFetecher);
-
   const ListBar = useMemo(() => {
     if (!roomListSWR || !roomListSWR.data) {
-      return <Text style={{color: '#ffffff'}}>No room list</Text>;
+      return <Text style={{color: '#ffffff'}}>No Room</Text>;
     }
     return roomListSWR.data.map((item, index) => (
-      <RoomListBar key={index} data={item} />
+      <Selectable key={index} id={item.id} type={ROOM}>
+        <RoomListBar data={item} />
+      </Selectable>
     ));
   }, [roomListSWR.data]);
 
--- src/components/Navigator/Screen/User.tsx
+++ src/components/Navigator/Screen/User.tsx
@@ -1,26 +1,28 @@
-import {View, Text, StyleSheet, GestureResponderEvent} from 'react-native';
+import {View, Text} from 'react-native';
 
-import {useAppDispatch, useAppSelector} from '@/redux/hooks';
-import {getSelectedUser, setUser} from '@/redux/features/userState';
-import NewUserModal from '@/components/Modal/User/UserAddModal';
+import {useAppSelector} from '@/redux/hooks';
+
+import UserAddModal from '@/components/Modal/User/UserAddModal';
 import useSWR from 'swr';
 import {UserListFetcher, UserListURL} from '@/swr/userSWR';
 import {useEffect, useMemo} from 'react';
-import {getUserAddModalVisible} from '@/redux/features/modalState';
-import {screenStyles} from '@/assets/screen/ScreenStylyeSheet';
+import {getUserAddModalVisible} from '@/redux/features/modal/modalState';
+import {screenStyles} from '@/assets/screen/ScreenStyleSheet';
 import UserListBar from '@/assets/list/data/user/UserListBar';
+import {USER} from '@/assets/static/texts/DataTypes';
+import Selectable from '@/assets/list/data/Selectable';
 
 const UserScreen = ({route}: {route: any}) => {
-  const dispatch = useAppDispatch();
   const modalVisible = useAppSelector(getUserAddModalVisible);
-  const user = useAppSelector(getSelectedUser);
   const userListSWR = useSWR(UserListURL, UserListFetcher);
   const ListBar = useMemo(() => {
     if (!userListSWR || !userListSWR.data) {
-      return <Text style={{color: '#ffffff'}}>No user list swr</Text>;
+      return <Text style={{color: '#ffffff'}}>No User</Text>;
     }
     return userListSWR.data.map((item, index) => (
-      <UserListBar key={index} data={item} />
+      <Selectable key={index} id={item.id} type={USER}>
+        <UserListBar data={item} />
+      </Selectable>
     ));
   }, [userListSWR.data]);
   useEffect(() => {
@@ -29,7 +31,7 @@ const UserScreen = ({route}: {route: any}) => {
 
   return (
     <View style={screenStyles.container}>
-      <NewUserModal />
+      <UserAddModal />
       {ListBar}
     </View>
   );
