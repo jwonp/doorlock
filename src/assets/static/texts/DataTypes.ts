f680524ebba27f32c444392ec8bb52abfa1b4aa9
Joowon <tkdel222@gmail.com>
2023-07-28 21:01:37 +0900

    update scan reservation text

--- src/assets/static/texts/DataTypes.ts
+++ src/assets/static/texts/DataTypes.ts
@@ -2,12 +2,13 @@ export const CARD = 'card';
 export const USER = 'user';
 export const ROOM = 'room';
 export const RESERVATION = 'reservation';
+export const SCAN = 'scan';
 
 const dataTypeSet = {
   card: CARD,
   user: USER,
   room: ROOM,
-  reservation:RESERVATION
+  reservation: RESERVATION,
 } as const;
 
 export type DataTypes = (typeof dataTypeSet)[keyof typeof dataTypeSet];
