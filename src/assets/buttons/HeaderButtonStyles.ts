2060f96a94f8bb04f50619ee837270c66aa0dac7
Joowon <tkdel222@gmail.com>
2023-07-28 20:59:48 +0900

    add header button styles

---
+++ src/assets/buttons/HeaderButtonStyles.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export const headerButtonStyles = StyleSheet.create({
+  marginRight: {
+    marginRight: '16%',
+  },
+  marginLeft: {
+    marginLeft: '22%',
+  },
+  icon: {width: 27, height: 27},
+  iconSmall: {width: 22, height: 22},
+});
