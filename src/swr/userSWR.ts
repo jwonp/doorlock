d822a2a05f5137b449dce41b7ae47b7846f8556b
Joowon <tkdel222@gmail.com>
2023-08-12 10:14:01 +0900

    update SelectedUserFetcher return type

--- src/swr/userSWR.ts
+++ src/swr/userSWR.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 import Config from 'react-native-config';
-
 import {UserListResponse} from '@/assets/models/dto/user/UserResponse';
-import {UserDataState} from '@/redux/features/selected/userState';
 import {request} from '@/util/request/controller/ApiController';
+import {User} from '@/assets/models/entity/User';
 export const UserListFetcher = (url: string): Promise<UserListResponse[]> =>
   request.get(url).then(res => res.data);
 
@@ -11,8 +10,9 @@ export const UserListURL = () => {
   return `${Config.BACKEND_ENDPOINT}/user/list`;
 };
 
-export const SelectedUserFetcher = (url: string): Promise<UserDataState> =>
-  axios.get(url).then(res => res.data);
+export const SelectedUserFetcher = (
+  url: string,
+): Promise<Omit<User, 'password'>> => axios.get(url).then(res => res.data);
 
 export const selectUserURL = (userId: string) => {
   return `${Config.BACKEND_ENDPOINT}/user?id=${userId}`;
