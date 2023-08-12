2553687ffc17844c0b3406b7d2579a2aef97b5dc
Joowon <tkdel222@gmail.com>
2023-08-12 10:15:16 +0900

    update request addRoom

--- src/util/request/room.ts
+++ src/util/request/room.ts
@@ -1,13 +1,20 @@
+import {RoomAddRequest} from '@/assets/models/dto/room/RoomRequest';
 import {request} from '@/util/request/controller/ApiController';
-export const addRoom =async()=>{
-    
-} 
-export const deleteSelectedRooms = async(selectedRoomIDList:number[]) =>{
-    if(!selectedRoomIDList || selectedRoomIDList.length < 1) {
-        return;
-    }
-    const roomListToDelete = {
-        idList:selectedRoomIDList
-    }
-    return await request.delete('/room',{data:roomListToDelete});
+export const addRoom = async (address: string) => {
+  if (!address) {
+    return;
+  }
+  const RoomAddData: RoomAddRequest = {
+    address: address,
+  };
+  return await request.post('/room', RoomAddData);
+};
+export const deleteSelectedRooms = async (selectedRoomIDList: number[]) => {
+  if (!selectedRoomIDList || selectedRoomIDList.length < 1) {
+    return;
+  }
+  const roomListToDelete = {
+    idList: selectedRoomIDList,
+  };
+  return await request.delete('/room', {data: roomListToDelete});
 };
