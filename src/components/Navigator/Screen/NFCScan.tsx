adf8cfbc082458d867ea4e520c38cf3353ccc340
Joowon <tkdel222@gmail.com>
2023-08-12 10:11:31 +0900

    update logic about scaning card

--- src/components/Navigator/Screen/NFCScan.tsx
+++ src/components/Navigator/Screen/NFCScan.tsx
@@ -1,84 +1,50 @@
 import React, {useState} from 'react';
 import {View, Text, Pressable, StyleSheet} from 'react-native';
 import NfcManager, {NfcTech, TagEvent} from 'react-native-nfc-manager';
-import {addCard} from '@/util/request/card';
-import { authorizeCard } from '@/util/request/auth';
-import { AxiosResponse } from 'axios';
-import { CardAuthorizedResponse } from '@/assets/models/dto/auth/AuthResponse';
-import { ADMIN } from '@/assets/static/texts/AuthorizeResults';
+
+import {authorizeCard} from '@/util/request/auth';
+import {AxiosResponse} from 'axios';
+import {CardAuthorizedResponse} from '@/assets/models/dto/auth/AuthResponse';
+import {ADMIN} from '@/assets/static/texts/AuthorizeResults';
+import {useAppDispatch} from '@/redux/hooks';
+import {DataType, setScreen} from '@/redux/features/modal/screenState';
 
 // Pre-step, call this before any NFC operations
 NfcManager.start();
 const NFCScanScreen = ({navigation}: {navigation: any}) => {
-  const [tag, setTag] = useState<TagEvent>(null);
-  const [result, setResult] = useState<String>('');
+  const dispatch = useAppDispatch();
+  const [isEnabled, setEnabled] = useState(false);
   const readNdef = async () => {
+    setEnabled(true);
     try {
       // register for the NFC tag with NDEF in it
       await NfcManager.requestTechnology(NfcTech.Ndef);
-
       // the resolved tag object will contain `ndefMessage` property
       const tag = await NfcManager.getTag();
 
-      authorizeCard(tag.id).then((res:AxiosResponse<CardAuthorizedResponse>)=>{
-        if(res.data.result === ADMIN){
-          navigation.navigate('Card')
+      authorizeCard(tag.id).then(res => {
+        if (res.data.result === ADMIN) {
+          dispatch(setScreen(DataType.card));
+          navigation.navigate('card');
         }
       });
-      setTag(tag);
-      
     } catch (ex) {
-      console.warn('Oops!', ex);
+      console.log('fail to tag', ex);
+      setEnabled(false);
     } finally {
       // stop the nfc scanning
       NfcManager.cancelTechnologyRequest();
+      setEnabled(false);
     }
   };
 
   return (
     <View style={styles.wrapper}>
       <Pressable onPress={readNdef}>
-        <Text style={styles.text}>Scan a Tag</Text>
+        <Text style={styles.text}>
+          {isEnabled ? 'Tag your card' : 'Press to start'}
+        </Text>
       </Pressable>
-      <Text style={styles.text}>{result}</Text>
-      {tag === null ? (
-        <Text style={styles.text}>NO DATA</Text>
-      ) : (
-        <View>
-          <Text style={styles.text}>{tag.id}</Text>
-          <Text style={styles.text}>{tag.maxSize}</Text>
-          <Text style={styles.text}>NDEF Message</Text>
-          {tag?.ndefMessage ? (
-            tag?.ndefMessage.map((message,index) => {
-              return (
-                <View key={index}>
-                  <Text style={styles.text}>{message.id}</Text>
-                  <Text style={styles.text}>{message.payload}</Text>
-                  <Text style={styles.text}>{message.tnf}</Text>
-                  <Text style={styles.text}>{message.type}</Text>
-                </View>
-              );
-            })
-          ) : (
-            <Text style={styles.text}>NO NDEF MESSAGE</Text>
-          )}
-          <Text style={styles.text}>Tech Types</Text>
-          {tag?.techTypes ? (
-            tag?.techTypes.map((type,index) => {
-              return (
-                <View key={index}>
-                  <Text style={styles.text}>{type}</Text>
-                </View>
-              );
-            })
-          ) : (
-            <Text style={styles.text}>NO TECH TYPES</Text>
-          )}
-
-          <Text style={styles.text}>Type</Text>
-          <Text style={styles.text}>{tag?.type}</Text>
-        </View>
-      )}
     </View>
   );
 };
