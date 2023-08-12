814494d030d1393d37fe8b5931ebf8d72962494a
Joowon <tkdel222@gmail.com>
2023-08-12 10:14:52 +0900

    update request path

--- src/util/request/auth.ts
+++ src/util/request/auth.ts
@@ -1,5 +1,5 @@
 import {request} from './controller/ApiController';
 import Config from 'react-native-config';
-export const checkAdminCard = async (id: string) => {
-  return await request.get(`${Config.BACKEND_ENDPOINT}/auth/admin?id=${id}`);
+export const authorizeCard = async (id: string) => {
+  return await request.get(`${Config.BACKEND_ENDPOINT}/auth/card?id=${id}`);
 };
