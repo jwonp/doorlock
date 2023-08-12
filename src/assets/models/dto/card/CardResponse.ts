4a4160ea44ddd944f9b3349f2cca5e9af045dcfe
Joowon <tkdel222@gmail.com>
2023-08-12 10:06:49 +0900

    update LastTagged property

--- src/assets/models/dto/card/CardResponse.ts
+++ src/assets/models/dto/card/CardResponse.ts
@@ -1,13 +1,13 @@
-import { TechType } from "../../types/TechType";
+import {TechType} from '../../types/TechType';
 import {CardDataState} from '../../../../redux/features/selected/cardState';
 
- export interface CardAddResponse {
-    id:string,
-    maxSize:number,
-    type:string,
-    techType:TechType
+export interface CardAddResponse {
+  id: string;
+  maxSize: number;
+  type: string;
+  techType: TechType;
+  lastTagged: string;
 }
-
 
 export interface CardWithReservationResponse extends CardDataState {
   reservationId: number;
