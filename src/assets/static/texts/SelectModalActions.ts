ea7c92eec082de796d1ae138ab92c3308cabda7e
Joowon <tkdel222@gmail.com>
2023-08-14 16:07:24 +0900

    update ActionSet property scan

--- src/assets/static/texts/SelectModalActions.ts
+++ src/assets/static/texts/SelectModalActions.ts
@@ -1,8 +1,12 @@
-export const ADD = "add";
-export const EDIT = "edit";
+import {SCAN} from './DataTypes';
+
+export const ADD = 'add';
+export const EDIT = 'edit';
+
 const ActionSet = {
-    add:ADD,
-    edit:EDIT
+  add: ADD,
+  edit: EDIT,
+  scan: SCAN,
 } as const;
 
-export type Actions = typeof ActionSet[keyof typeof ActionSet];
+export type Actions = (typeof ActionSet)[keyof typeof ActionSet];
