887d61bd5de018df1711391f1d2fbd170e742577
Joowon <tkdel222@gmail.com>
2023-08-17 22:20:42 +0900

    update check modal to check out

--- src/components/Modal/Reservation/ReservationEditModal.tsx
+++ src/components/Modal/Reservation/ReservationEditModal.tsx
@@ -1,4 +1,4 @@
-import {Modal, View, Text, Pressable, Image} from 'react-native';
+import {Modal, View, Text, Pressable, Image, StyleSheet} from 'react-native';
 import {useAppDispatch, useAppSelector} from '@/redux/hooks';
 import {
   getReservationEditModalVisible,
@@ -14,6 +14,8 @@ import {
   ReservationAddUserURL,
   ReservationDetailFetcher,
   ReservationDetailURL,
+  ReservationFetcher,
+  ReservationURL,
 } from '@/swr/reservationSWR';
 import {ReservationFullResponse} from '@/assets/models/dto/reservation/ReservationResponse';
 import ModalDataContainer from '@/assets/list/data/modal/ModalDataContainer';
@@ -28,9 +30,13 @@ import {
 } from '@/redux/features/modal/data/reservationEditState';
 import {modalHeaderStlyes, modalStyles} from '@/assets/modals/ModalStyles';
 import CloseIcon from '@/public/close-white.png';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {ReservationModfiyRequest} from '@/assets/models/dto/reservation/ReservationRequest';
-import {modifyReservation} from '@/util/request/reservation';
+import {
+  checkOut,
+  modifyReservation,
+  setCheckIn,
+} from '@/util/request/reservation';
 import {AxiosError} from 'axios';
 import {ScrollView} from 'react-native';
 const ReservationEditModal = () => {
@@ -40,6 +46,7 @@ const ReservationEditModal = () => {
   const userId = useAppSelector(getReservationEditUserId);
   const cardId = useAppSelector(getReservationEditCardId);
   const roomId = useAppSelector(getReservationEditRoomId);
+  const reservationSWR = useSWR(ReservationURL, ReservationFetcher);
   const reservationDetailSWR = useSWR(
     ReservationDetailURL(reservationEditId),
     ReservationDetailFetcher,
@@ -74,19 +81,51 @@ const ReservationEditModal = () => {
   useEffect(() => {
     roomSWR.mutate();
   }, [roomId]);
+
   const closeModal = () => {
     dispatch(resetEditIds());
     dispatch(setReservationEditModalVisible(false));
   };
+
+  const [checkModalVisible, setCheckModalVisible] = useState<boolean>(false);
   return (
     <Modal
       visible={modalVisible}
-      transparent={true}
+      transparent={false}
       animationType={SLIDE}
       onRequestClose={() => {
         closeModal();
       }}>
-      <View style={modalStyles.container}>
+      <Modal
+        visible={checkModalVisible}
+        transparent={true}
+        animationType={'fade'}>
+        <View style={checkModalVisible && checkModalStyles.background}></View>
+        <View style={checkModalStyles.container}>
+          <View style={checkModalStyles.submit}>
+            <Pressable
+              onPress={() => {
+                checkOut(reservationDetailSWR.data.id).then(() => {
+                  setCheckModalVisible(false);
+                  reservationSWR.mutate();
+                  closeModal();
+                });
+              }}>
+              <Text style={checkModalStyles.text}>Check Out</Text>
+            </Pressable>
+          </View>
+          <View style={checkModalStyles.cancel}>
+            <Pressable
+              onPress={() => {
+                setCheckModalVisible(false);
+              }}>
+              <Text style={checkModalStyles.text}>Cancel</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+      <ScrollView style={modalStyles.container}>
         <View style={modalStyles.header}>
           <View style={modalHeaderStlyes.closeContainer}>
             <Pressable
@@ -124,6 +163,46 @@ const ReservationEditModal = () => {
 
         <View>
           <View style={modalStyles.main}>
+            <View style={buttonStyles.container}>
+              <View style={detail.isCheckedIn && buttonStyles.hidden}>
+                <View style={buttonStyles.box}>
+                  <View style={buttonStyles.card}>
+                    <Pressable
+                      onPress={() => {
+                        setCheckIn(detail.id, true).then(() => {
+                          reservationDetailSWR.mutate();
+                          reservationSWR.mutate();
+                        });
+                      }}>
+                      <Text style={buttonStyles.text}>Check In</Text>
+                    </Pressable>
+                  </View>
+                </View>
+              </View>
+              <View style={!detail.isCheckedIn && buttonStyles.hidden}>
+                <View style={buttonStyles.box}>
+                  <View style={buttonStyles.card}>
+                    <Pressable
+                      onPress={() => {
+                        setCheckModalVisible(true);
+                      }}>
+                      <Text style={buttonStyles.text}>Check Out</Text>
+                    </Pressable>
+                  </View>
+                  <View style={buttonStyles.card}>
+                    <Pressable
+                      onPress={() => {
+                        setCheckIn(detail.id, false).then(() => {
+                          reservationDetailSWR.mutate();
+                          reservationSWR.mutate();
+                        });
+                      }}>
+                      <Text style={buttonStyles.text}>Cancel Check In</Text>
+                    </Pressable>
+                  </View>
+                </View>
+              </View>
+            </View>
             <ModalDataContainer>
               <ModalDataView
                 title={'Reservation ID'}
@@ -132,7 +211,7 @@ const ReservationEditModal = () => {
             </ModalDataContainer>
             <ModalDataContainer tab={1}>
               <ModalDataCategoryView type={USER} />
-              <ModalDataView title={'User ID'} text={userId} />
+              <ModalDataView title={'User ID'} text={detail.userId} />
               <ModalDataView
                 title={'Name'}
                 text={userSWR && userSWR.data ? userSWR.data.name : detail.name}
@@ -146,11 +225,14 @@ const ReservationEditModal = () => {
             </ModalDataContainer>
             <ModalDataContainer tab={1}>
               <ModalDataCategoryView type={CARD} />
-              <ModalDataView title={'Card ID'} text={cardId} />
+              <ModalDataView title={'Card ID'} text={detail.cardId} />
             </ModalDataContainer>
             <ModalDataContainer tab={1}>
               <ModalDataCategoryView type={ROOM} />
-              <ModalDataView title={'Room ID'} text={roomId.toString()} />
+              <ModalDataView
+                title={'Room ID'}
+                text={detail.roomId.toString()}
+              />
               <ModalDataView
                 title={'Address'}
                 text={
@@ -168,9 +250,70 @@ const ReservationEditModal = () => {
             </ModalDataContainer>
           </View>
         </View>
-      </View>
+      </ScrollView>
     </Modal>
   );
 };
+const checkModalStyles = StyleSheet.create({
+  container: {
+    marginTop: 'auto',
+    height: '100%',
+    paddingBottom: '7%',
+  },
+  submit: {
+    marginTop: 'auto',
+    marginBottom: '7%',
+    paddingTop: '1%',
+    paddingBottom: '1%',
+    width: '100%',
+    backgroundColor: '#D7C0AE',
+    borderRadius: 14,
+  },
+  cancel: {
+    width: '100%',
+    backgroundColor: '#D7C0AE',
+    paddingTop: '1%',
+    paddingBottom: '1%',
+    borderRadius: 14,
+  },
+  text: {color: '#000000', textAlign: 'center', fontSize: 24},
 
+  background: {
+    height: '100%',
+    backgroundColor: '#967E76',
+    shadowOpacity: 0.5,
+    opacity: 0.5,
+  },
+});
+const buttonStyles = StyleSheet.create({
+  container: {
+    paddingLeft: '5%',
+    paddingRight: '5%',
+    paddingTop: '2%',
+    paddingBottom: '2%',
+  },
+  box: {
+    width: '100%',
+  },
+  card: {
+    width: '100%',
+    backgroundColor: '#EEE3CB',
+    paddingLeft: '4%',
+    paddingRight: '4%',
+    paddingTop: '2%',
+    paddingBottom: '2%',
+    marginRight: '3%',
+    marginBottom: '2%',
+    borderRadius: 6,
+  },
+  text: {
+    fontSize: 16,
+    color: '#000000',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  hidden: {
+    display: 'none',
+  },
+});
 export default ReservationEditModal;
