02e23665274a56f84453ba07fb59d9e641ba0779
Joowon <tkdel222@gmail.com>
2023-09-17 21:55:04 +0900

    update request to just axios

--- src/util/request/auth.ts
+++ src/util/request/auth.ts
@@ -1,11 +1,12 @@
-import {request} from './controller/ApiController';
+import axios from 'axios';
+
 import Config from 'react-native-config';
 export const authorizeCard = async (id: string, address: string) => {
   const authorizeData = {
     cardId: id,
     address: address,
   };
-  return await request.post(
+  return await axios.post(
     `${Config.BACKEND_ENDPOINT}/auth/card`,
     authorizeData,
   );
